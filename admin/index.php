bedb60f3bfe63a82215a092a449eaceca149349d
igor94grozdanic <igor94grozdanic@gmail.com>
2024-11-30 16:53:35 +0100

    Create new property finished

--- admin/index.php
+++ admin/index.php
@@ -18,112 +18,122 @@ if (!isset($_SESSION["user"])) {
     <div  style="position:absolute; top: 0; right:0;">
         <a href="logout.php" class="btn btn-warning" >Logout</a>
     </div>
-    <div class="container">
+    <div class="container col-lg-12">
         <?php
-        if (isset($_POST["submit1"])) {
+        if (isset($_POST["createProperty"])) {
+           $ref = $_POST["ref"];
+           $proName = $_POST["fullname"];
+           $proAlias = 'Test';
+           $agentId = 1;
+           $companyId = 0;
+
+           $price = $_POST["price"];
+           $proSmallDesc = $_POST["description"];
+           $proType =  $_POST["propertyTypes"];
+           $soldOn = '2024-11-30';
+
+           $address = $_POST["address"];
+           $metaDesc = $_POST["description1"];
+           $created = '2024-11-30';
+           $createdBy = $_GET['userId'];
+           $removeDate = '2024-11-30';
+
+           $proPdfFile = '';
+           $energy = '0.00';
+           $climate = '0.00';
+           $rentTime = '';
+
+           $squareFeet = '0.00';
+           $lotSize = '0.00';
+           $cclass = '';
+           $eclass = '';
+
            $selectedCategoryId = $_POST["propertyCategories"];
-           $selectedTypeId =  $_POST["propertyTypes"];
-
-           $fullName = $_POST["fullname"];
-           $userName = $_POST["username"];
-           $email = $_POST["email"];
-           $password = $_POST["password"];
-           $passwordRepeat = $_POST["repeat_password"];
-           $params = "{"."admin_style".":".","."admin_language".":".","."language".":".","."editor".":".","."helpsite".":".","."timezone".":"."}";
-           
-           $passwordHash = password_hash($password, PASSWORD_DEFAULT);
-
-           $errors = array();
-           
-           if (empty($fullName) OR empty($email) OR empty($password) OR empty($passwordRepeat)) {
-            array_push($errors,"All fields are required");
-           }
-           if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            array_push($errors, "Email is not valid");
-           }
-           if (strlen($password)<8) {
-            array_push($errors,"Password must be at least 8 charactes long");
-           }
-           if ($password!==$passwordRepeat) {
-            array_push($errors,"Password does not match");
-           }
-           require_once "database.php";
-           $sql = "SELECT * FROM jos_users WHERE email = '$email'";
-           $result = mysqli_query($conn, $sql);
-           $rowCount = mysqli_num_rows($result);
-           if ($rowCount>0) {
-            array_push($errors,"Email already exists!");
-           }
-           if (count($errors)>0) {
-            foreach ($errors as  $error) {
-                echo "<div class='alert alert-danger'>$error</div>";
-            }
-           }else{
             
-            $sql = "INSERT INTO jos_users (name, username, email, password, params) VALUES ( ?, ?, ? ,?, ?)";
+            require_once "database.php";
+            $sql = "INSERT INTO jos_osrs_properties (
+                                                      ref, pro_name, pro_alias, agent_id, company_id, price,
+                                                      pro_small_desc, pro_type, soldOn, address, metadesc,
+                                                      created, created_by, remove_date, pro_pdf_file, energy,
+                                                      climate, rent_time, square_feet, lot_size, c_class, e_class
+                                                    ) 
+                                                    VALUES ( ?, ?, ? ,?, ?, ?, ?, ? ,?, ?, ?, ?, ? ,?, ?, ?, ?, ? ,?, ?, ?, ?)";
             $stmt = mysqli_stmt_init($conn);
             $prepareStmt = mysqli_stmt_prepare($stmt,$sql);
             if ($prepareStmt) {
-                mysqli_stmt_bind_param($stmt,"sssss",$fullName, $userName, $email, $passwordHash, $params);
+                mysqli_stmt_bind_param($stmt,"ssssssssssssssssssssss",$ref, $proName, $proAlias, $agentId, $companyId, $price, 
+                                                   $proSmallDesc, $proType, $soldOn, $address, $metaDesc,
+                                                   $created, $createdBy, $removeDate, $proPdfFile, $energy,
+                                                   $climate, $rentTime, $squareFeet, $lotSize, $cclass, $eclass);
                 mysqli_stmt_execute($stmt);
-                echo "<div class='alert alert-success'>You are registered successfully.</div>";
-            }else{
+                echo "<div class='alert alert-success'>Nova nekretnina je uspešno kreirana</div>";
+            }
+            else{
                 die("Something went wrong");
             }
-           }
         }
         ?>
-        <form action="registration.php" method="post">
-            <div class="form-group">
-                <input type="text" class="form-control" name="ref" placeholder="Ref:">
+        <form class="col-lg-12" action="index.php?userId=".$createdBy method="post">
+            <div class="col-lg-12 d-flex">
+                <div class="col-lg-6 form-group d-inline-block">
+                    <input type="text" class="form-control" name="ref" placeholder="Ref:">
+                </div>
+                <div class="col-lg-6 form-group d-inline-block" style="margin-left:20px;">
+                    <input type="text" class="form-control" name="fullname" placeholder="Naziv:">
+                </div>
             </div>
-            <div class="form-group">
-                <input type="text" class="form-control" name="fullname" placeholder="Naziv:">
-            </div>
-            <div class="form-group">
-                <select name="propertyCategories">
-                    <?php 
-                       require_once "database.php";
-                       $sql = "SELECT id, category_name FROM marinkom_jos1.jos_osrs_categories";
-                       $result = mysqli_query($conn, $sql);
-                       while($rows = $result->fetch_assoc()){
-                        $categoryName = $rows['category_name'];
-                        $categoryId = $rows['id'];
+            <div class="col-lg-12 d-flex">
+                <div class="col-lg-6 form-group d-inline-block">
+                    <select name="propertyCategories" class="form-select">
+                        <?php 
+                           require_once "database.php";
+                           $sql = "SELECT id, category_name FROM marinkom_jos1.jos_osrs_categories";
+                           $result = mysqli_query($conn, $sql);
+                           
+                           while($rows = $result->fetch_assoc()){
+                               $categoryName = $rows['category_name'];
+                               $categoryId = $rows['id'];
 
-                        echo "<option value='$categoryId'>$categoryName</option>";
-                       };
-                    ?>
-                </select>
-            </div>
-            <div class="form-group">
-                <select name="propertyTypes">
-                    <?php 
-                       require_once "database.php";
-                       $sql = "SELECT id, type_name FROM marinkom_jos1.jos_osrs_types";
-                       $result = mysqli_query($conn, $sql);
-                       while($rows = $result->fetch_assoc()){
-                        $typeName = $rows['type_name'];
-                        $typeId = $rows['id'];
+                               echo "<option value='$categoryId'>$categoryName</option>";
+                           };
+                        ?>
+                    </select>
+                </div>
+                <div class="col-lg-6 form-group d-inline-block" style="margin-left: 20px;">
+                    <select name="propertyTypes" class="form-select">
+                        <?php 
+                           require_once "database.php";
+                           $sql = "SELECT id, type_name FROM marinkom_jos1.jos_osrs_types";
+                           $result = mysqli_query($conn, $sql);
+                           
+                           while($rows = $result->fetch_assoc()){
+                               $typeName = $rows['type_name'];
+                               $typeId = $rows['id'];
 
-                        echo "<option value='$typeId'>$typeName</option>";
-                       };
-                    ?>
-                </select>
+                               echo "<option value='$typeId'>$typeName</option>";
+                           };
+                        ?>
+                    </select>
+                </div>
             </div>
-            <div class="form-group">
-                <input type="text" class="form-control" name="username" placeholder="Username:">
+            <div class="col-lg-12 d-flex">
+                <div class="col-lg-6 form-group d-inline-block">
+                    <input type="text" class="form-control" name="price" placeholder="Cena:">
+                </div>
+                <div class="col-lg-6 form-group d-inline-block" style="margin-left: 20px;">
+                    <input type="text" class="form-control" name="address" placeholder="Adresa:">
+                </div>
             </div>
-            <div class="form-group">
-                <input type="emamil" class="form-control" name="email" placeholder="Email:">
-            </div>
-            <div class="form-group">
-                <input type="password" class="form-control" name="password" placeholder="Password:">
-            </div>
-            <div class="form-group">
-                <input type="password" class="form-control" name="repeat_password" placeholder="Repeat Password:">
+            <div class="col-lg-12 d-flex">
+                <div class="col-lg-6 form-group d-inline-block">
+                   <textarea type="text" class="form-control" name="description" placeholder="Opis:"></textarea>
+                </div>
+                <div class="col-lg-6 form-group d-inline-block" style="margin-left: 20px;">
+                   <textarea type="text" class="form-control" name="description1" placeholder="Beleška za agenta:"></textarea>
+                </div>
             </div>
             <div class="form-btn">
-                <input type="submit" class="btn btn-primary" value="Kreiraj nekretninu" name="submit1">
+                <input type="submit" value="Kreiraj nekretninu" name="createProperty" class="btn btn-primary">
             </div>
         </form>
     </div>
--- admin/login.php
+++ admin/login.php
@@ -28,7 +28,7 @@ if (isset($_SESSION["user"])) {
                 if (password_verify($password, $user["password"])) {
                     session_start();
                     $_SESSION["user"] = "yes";
-                    header("Location: index.php");
+                    header("Location: index.php?userId=".$user["id"]);
                     die();
                 }else{
                     echo "<div class='alert alert-danger'>Password does not match</div>";
