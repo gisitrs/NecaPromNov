8eccf31d63cce8af213fc8e966be8e6c211d7e4a
igor94grozdanic <igor94grozdanic@gmail.com>
2025-04-22 22:20:57 +0200

    View updated

--- assets/Database/vw_getallimages.sql
+++ assets/Database/vw_getallimages.sql
@@ -4,6 +4,7 @@ CREATE OR REPLACE
     SQL SECURITY DEFINER
 VIEW `marinkom_jos1`.`vw_getallimages` AS
     SELECT 
+        `marinkom_jos1`.`jos_osrs_photos`.`id` AS `photo_id`,
         `marinkom_jos1`.`jos_osrs_properties`.`id` AS `id`,
         `marinkom_jos1`.`jos_osrs_properties`.`ref` AS `ref`,
         `marinkom_jos1`.`jos_osrs_properties`.`pro_name` AS `pro_name`,
